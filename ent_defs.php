fa5094ecf09a21c9f8b1305649a1a8a7776894c5
taisson <thomas.jaisson@ac-paris.fr>
2016-08-26 13:42:21 +0200

    WOP

--- ent_defs.php
+++ ent_defs.php
@@ -44,11 +44,11 @@ require_once(__DIR__ . '/ents/base.php');
  *
  * toujoursn appeler end_register() à la fin
  */
-//require_once(__DIR__ . '/ents/local.php');
-//auth_entsync_ent_base::register('local', 4);
+require_once(__DIR__ . '/ents/local.php');
+auth_entsync_ent_base::register('local', 4);
 
 require_once(__DIR__ . '/ents/ents_paris.php');
-//auth_entsync_ent_base::register('pcn', 2);
+auth_entsync_ent_base::register('pcn', 2);
 auth_entsync_ent_base::register('monlyceenet', 1);
 auth_entsync_ent_base::register('ng', 3);
 
