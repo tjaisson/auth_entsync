5dd5d4085f8fde97239ec98407042edf959fca6a
taisson <thomas.jaisson@ac-paris.fr>
2016-09-05 16:23:51 +0200

    File TMP store

--- lib/tmpstore.php
+++ lib/tmpstore.php
@@ -32,7 +32,7 @@ abstract class auth_entsync_tmpstore {
     
     public function __construct($storecode = null) {}
     public static function get_store($storecode = null) {
-        return new auth_entsync_dbtmpstore($storecode);
+        return new auth_entsync_fstmpstore($storecode);
     }
     public abstract function save();
     public abstract function add_ius($ius);
@@ -40,6 +40,63 @@ abstract class auth_entsync_tmpstore {
     public abstract function clear();
     public abstract function count();
 }
+
+class auth_entsync_fstmpstore extends auth_entsync_tmpstore {
+    protected $_storecode;
+    protected $_tmparray;
+
+    public function __construct($storecode = null) {
+        $this->_storecode = $storecode;
+        if($storecode) {
+            $dir = make_temp_directory('entsync');
+            $file = $dir . '/' . $storecode;
+            $this->_tmparray = json_decode(file_get_contents($file), true);
+        } else  {
+            $this->_tmparray = array();
+        }
+    }
+    public function save() {
+        $dir = make_temp_directory('entsync');
+        if($this->_storecode) {
+            $file = $dir . '/' . $this->_storecode;
+        } else  {
+            $i = 100;
+            while(file_exists($dir . '/' . $i)) {
+                ++$i;
+            }
+            $this->_storecode = $i;
+            $file = $dir . '/' . $this->_storecode;
+        }
+        file_put_contents($file, json_encode($this->_tmparray));
+        return $this->_storecode;
+    }
+    public function add_ius($ius) {
+        if(empty($ius)) return;
+        $this->_progressreporter->start_progress('Préparation',count($ius),1);
+        $i = 1;
+        while($ius) {
+            $iu = array_pop($ius);
+            $this->_progressreporter->progress($i++);
+            $this->_tmparray[$iu->uid] = $iu;
+        }
+        $this->_progressreporter->end_progress();
+    }
+    public function get_ius() {
+        return $this->_tmparray;
+    }
+    public function clear() {
+        if($this->_storecode) {
+            $dir = make_temp_directory('entsync');
+            $file = $dir . '/' . $this->_storecode;
+            unlink($file);
+        }
+    }
+    public function count() {
+        return count($this->_tmparray);
+    }
+}
+
+
 
 class auth_entsync_dbtmpstore extends auth_entsync_tmpstore {
     protected $_storecode;
