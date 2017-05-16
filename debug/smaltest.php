10561c7be6897f9646109204b727ea8835f90bb3
taisson <thomas.jaisson@ac-paris.fr>
2017-05-16 19:12:07 +0200

    test

---
+++ debug/smaltest.php
@@ -0,0 +1,63 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Gestion des utilisateurs.
+ *
+ * @package auth_entsync
+ * @copyright 2016 Thomas Jaisson
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ */
+
+
+//require(__DIR__ . '/../../../config.php');
+
+function parse_size($size) {
+	$unit = preg_replace('/[^bkmgtpezy]/i', '', $size); // Remove the non-unit characters from the size.
+	$size = preg_replace('/[^0-9\.]/', '', $size); // Remove the non-numeric characters from the size.
+	if ($unit) {
+		// Find the position of the unit in the ordered string which is the power of magnitude to multiply a kilobyte by.
+		return round($size * pow(1024, stripos('bkmgtpezy', $unit[0])));
+	}
+	else {
+		return round($size);
+	}
+}
+
+function returnBytes($val) {
+	$val  = trim($val);
+	$last = strtolower($val[strlen($val) - 1]);
+	switch ($last) {
+		case 'g':
+			$val *= 1024;
+		case 'm':
+			$val *= 1024;
+		case 'k':
+			$val *= 1024;
+	}
+	
+	return $val;
+}
+
+//$val = ini_get('upload_max_filesize');
+$val = "200kK";
+
+echo $val . ' ';
+echo returnBytes($val) . ' ';
+echo parse_size($val) . ' ';
+
+
+die;
