b50cde3d52bdb723777d9a84dcb828a6d9480ea1
tjaisson <thomas.jaisson@ac-paris.fr>
2017-07-08 09:51:51 +0200

    Evol
    Multifile : collapse par défaut.
    Infos non données en cours de chargement

--- bulk.php
+++ bulk.php
@@ -210,8 +210,10 @@ if ($storeid) {
         $formparams['multi'] = $ent->accept_multifile($filetype);
         $mform = new bulk_form(null, $formparams);
         // On donne la possibilité d'envoyer un autre fichier mais le type de fichier ne doit pas changer.
+        $dispinfo = false;
     } else {
         $mform = new bulk_form(null, $formparams);
+        $dispinfo = true;
     }
 }
 
@@ -219,10 +221,11 @@ echo $OUTPUT->heading_with_help(get_string('entsyncbulk', 'auth_entsync'), 'ents
 
 $mform->display();
 
-$i = usertblhelper::count_users(1);
-$ii = usertblhelper::count_users(2);
-$iii = usertblhelper::count_users([1, 2]);
-echo "<ul><li>{$i} élèves</li><li>{$ii} enseignants</li><li>Total : {$iii}</li></ul>";
-
+if ($dispinfo) {
+    $i = usertblhelper::count_users(1);
+    $ii = usertblhelper::count_users(2);
+    $iii = usertblhelper::count_users([1, 2]);
+    echo "<ul><li>{$i} élèves</li><li>{$ii} enseignants</li><li>Total : {$iii}</li></ul>";
+}
 echo $OUTPUT->footer();
 die;
