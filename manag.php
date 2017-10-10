9923ca04ca5c1401d1cac6eea33375c5ba1c68f4
tjaisson <thomas.jaisson@ac-paris.fr>
2017-10-10 10:53:43 +0200

    add manag

---
+++ manag.php
@@ -0,0 +1,119 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Gestion des utilisateurs.
+ *
+ * @package auth_entsync
+ * @copyright 2016 Thomas Jaisson
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ */
+
+//TODO : gérer le format de cours par défaut
+
+require(__DIR__ . '/../../config.php');
+require_once($CFG->libdir . '/adminlib.php');
+require_once($CFG->libdir . '/moodlelib.php');
+require_once($CFG->dirroot.'/cohort/lib.php');
+require_once('ent_defs.php');
+
+$views = ['cohort'];
+$view = optional_param('view', 'none', PARAM_TEXT);
+if(!in_array($view, $views))
+{
+    redirect(new moodle_url('/'));
+}
+
+require_login();
+admin_externalpage_setup('authentsyncuser');
+require_capability('moodle/site:config', context_system::instance());
+
+$msg = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    //something posted
+    switch ($view) {
+        case 'cohort':
+            $msg = traite_cohort();
+            break;
+    }
+}
+
+echo $OUTPUT->header();
+
+if(!empty($msg)) {
+    echo $OUTPUT->notification($msg,
+        core\output\notification::NOTIFY_INFO);
+}
+
+switch ($view) {
+    case 'cohort':
+        affiche_cohort();
+        break;
+}
+
+echo $OUTPUT->footer();
+die;
+
+function affiche_cohort() {
+    global $OUTPUT, $DB;
+    $clist = $DB->get_records('cohort', ['component' => 'auth_entsync']);
+    $ct = new html_table();
+    $ct->head = ['Sélection', 'Nom', 'Identifiant cohorte'];
+    foreach($clist as $c) {
+        $cb = "<input type=\"checkbox\" name=\"selectc[]\" value=\"{$c->id}\"></input>";
+        $row = [$cb, $c->name, $c->idnumber];
+        $ct->data[] = new html_table_row($row);
+    }
+    echo $OUTPUT->heading('Gestion des cohortes');
+    ?>
+<form method="post">
+<h1>Cohortes gérées</h1>
+<p>
+Les cohortes suivantes sont gérées par le plugin 'entsync'.
+Vous pouvez ici les détacher du plugin pour pouvoir les gérer dans l'interface moodle.
+Les cohortes détachées ne seront plus synchronisées.
+</p>
+<?php
+    echo html_writer::table($ct);
+    ?>
+<input name="release" type="submit" value="Détacher" />
+</form>
+<?php
+}
+
+function traite_cohort() {
+    global $DB;
+    $msg = "";
+    if (isset($_POST['release'])) {
+        if(isset($_POST['selectc'])) {
+            $select = $_POST['selectc'];
+            if (count($select) > 0){
+                $msg = '<h2>Cohortes détachées</h2><p>';
+                foreach($select as $cid) {
+                    $c = $DB->get_record('cohort', ['id' => $cid]);
+                    $c->component = '';
+                    cohort_update_cohort($c);
+                    $msg .= $c->name . ' ';
+                }
+                $msg .= '</p>';
+            }
+        }
+    }
+    return $msg;
+}
+
+
