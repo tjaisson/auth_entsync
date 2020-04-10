b1fa1a5c57d9de991ff4ca23b907c4ae79086878
tjaisson <thomas.jaisson@ac-paris.fr>
2020-04-10 14:38:13 +0200

    updated admin jump process

--- jump/jump.php
+++ jump/jump.php
@@ -13,7 +13,6 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
 /**
  * Gestion des instances.
  *
@@ -21,27 +20,75 @@
  * @copyright 2016 Thomas Jaisson
  * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
  */
-
 require(__DIR__ . '/../../../config.php');
-
-if (!isloggedin()) {
-    die();
-}
-
-if (!is_siteadmin()) {
-    die();
-}
-
+use \auth_entsync\farm\instance;
+use \auth_entsync\farm\iic;
 $inst = optional_param('inst', null, PARAM_TEXT);
-
-if (!$inst) {
+if (!empty($inst)) {
+    if ((!isloggedin()) || (!is_siteadmin())) die();
+    $instance = instance::get_record(['dir' => $inst]);
+    if (!$instance) {
+        die();
+    }
+    $tk = iic::createToken($instance->dir, null, 5);
+    if (empty($tk)) die();
+    $redirecturl = new moodle_url($instance->wwwroot() . '/auth/entsync/jump/jump.php', ['tk' => $tk]);
+    redirect($redirecturl);
     die();
 }
-$instance = \auth_entsync\farm\instance::get_record(['dir' => $inst]);
-if (!$instance) {
-    die();
+$tk = optional_param('tk', null, PARAM_TEXT);
+if (empty($tk)) die();
+if (isloggedin() || instance::is_gw()) redirect($CFG->wwwroot);
+if (iic::OK !== iic::open($tk, instance::inst())) die();
+require_once($CFG->libdir.'/filelib.php');
+require_once($CFG->dirroot.'/user/lib.php');
+$mdlu = $DB->get_record('user', ['username' => 'pam.central.adm', 'auth' => 'entsync']);
+$_mdlu = new stdClass();
+$_mdlu->isdirty = false;
+if (!$mdlu) {
+    // L'utilisateur admin central n'existe pas.
+    $_mdlu->isdirty = true;
+    $_mdlu->mnethostid = $CFG->mnet_localhost_id;
+    $_mdlu->username = 'pam.central.adm';
+    $_mdlu->password = md5("entsync\\noPW");
+    $_mdlu->auth = 'entsync';
+    $_mdlu->confirmed = 1;
+    $_mdlu->firstname = '-';
+    $_mdlu->lastname = 'Administrateur Central PAM';
+    $_mdlu->email = 'ad@ac.invalid';
+    $_mdlu->emailstop = 1;
+    $_mdlu->lang = 'fr';
+    unset($_mdlu->isdirty);
+    $_mdlu->id = user_create_user($_mdlu, false, true);
+} else {
+    $_mdlu->id = $mdlu->id;
+    if ($mdlu->suspended) {
+        $_mdlu->suspended = 0;
+        $_mdlu->isdirty = true;
+    }
+    if (!$mdlu->confirmed) {
+        $_mdlu->confirmed = 1;
+        $_mdlu->isdirty = true;
+    }
+    if ($_mdlu->isdirty) {
+        unset($_mdlu->isdirty);
+        user_update_user($_mdlu, false, true);
+    }
 }
-$k = \auth_entsync\farm\iic::createToken(5, true);
-if (!$k) die();
-$redirecturl = new moodle_url($instance->wwwroot() . '/auth/entsync/jump/login.php', $k);
-redirect($redirecturl);
+$mdlu = $DB->get_record('user', ['id' => $_mdlu->id]);
+set_user_preference('auth_forcepasswordchange', false, $mdlu->id);
+if (!is_siteadmin($mdlu)) {
+    $mdluid = (int)$mdlu->id;
+    $admins = array();
+    foreach (explode(',', $CFG->siteadmins) as $admin) {
+        $admin = (int)$admin;
+        if ($admin) {
+            $admins[$admin] = $admin;
+        }
+    }
+    $admins[$mdluid] = $mdluid;
+    set_config('siteadmins', implode(',', $admins));
+}
+complete_user_login($mdlu);
+\core\session\manager::apply_concurrent_login_limit($mdlu->id, session_id());
+redirect($CFG->wwwroot);
