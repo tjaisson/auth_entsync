ef25580ee376115bd1e14259aab265073353913a
tjaisson <thomas.jaisson@ac-paris.fr>
2020-04-18 17:35:13 +0200

    Add cache for list_instances ajax script

--- list_instances.php
+++ list_instances.php
@@ -13,24 +13,37 @@ if (count($_GET) === 1)  {
     $adminMode = false;
 }
 require(__DIR__ . '/../../config.php');
+header('Content-type: application/json');
 
 if ($adminMode) {
     require_login();
     $sitecontext = context_system::instance();
     require_capability('moodle/site:config', $sitecontext);
+    echo json_encode(auth_entsync_list(true));
+    die();
 }
-header('Content-type: application/json');
-$entsync = \auth_entsync\container::services();
-$instances = $entsync->query('instances');
-$lst = [];
-$instancesList = $instances->get_instances([], 'dir');
-foreach ($instancesList as $instance) {
-    if (($adminMode) || ($instance->get('rne') !== '00')) {
-        $lst[] = ['dir' => $instance->get('dir'), 'name' => $instance->get('name')];
-    }
+$cache = \cache::make('auth_entsync', 'farm');
+if (false !== ($jsonList = $cache->get('instances'))) {
+    echo $jsonList;
+    die();
 }
-echo json_encode($lst);
+$jsonList = json_encode(auth_entsync_list());
+$cache->set('instances', $jsonList);
+echo $jsonList;
+die();
 function auth_entsync_error($code) {
     http_response_code($code);
     die();
+}
+function auth_entsync_list($admin = false) {
+    $entsync = \auth_entsync\container::services();
+    $instances = $entsync->query('instances');
+    $lst = [];
+    $instancesList = $instances->get_instances([], 'dir');
+    foreach ($instancesList as $instance) {
+        if (($admin) || ($instance->get('rne') !== '00')) {
+            $lst[] = ['dir' => $instance->get('dir'), 'name' => $instance->get('name')];
+        }
+    }
+    return $lst;
 }
