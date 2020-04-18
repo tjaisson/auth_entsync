36748a428cdd62ca4c3deab7ebb9a0c3990b4e01
tjaisson <thomas.jaisson@ac-paris.fr>
2020-04-18 18:00:49 +0200

    Bump version

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020041100;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2020041106;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2016051900;        // Requires this Moodle version
 $plugin->component = 'auth_entsync';    // Full name of the plugin (used for diagnostics).
