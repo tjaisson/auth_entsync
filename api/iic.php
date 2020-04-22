a9abc09ea730ea17fc420941aaefb1fa42d1c8bf
tjaisson <thomas.jaisson@ac-paris.fr>
2020-04-22 14:20:53 +0200

    tune

--- api/iic.php
+++ api/iic.php
@@ -2,7 +2,7 @@
 define('NO_DEBUG_DISPLAY', true);
 define('AJAX_SCRIPT', true);
 define('NO_MOODLE_COOKIE', true);
-equire(__DIR__ . '/../../../config.php');
+require(__DIR__ . '/../../../config.php');
 header('Content-type: application/json');
 $entsync = \auth_entsync\container::services();
 $server = $entsync->query('api_server');
